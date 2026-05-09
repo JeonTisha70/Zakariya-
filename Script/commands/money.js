9ff0f12818d391d9f96c08b748fc5051224b1907
JeonTisha70 <tishamyhappiness@gmail.com>
2026-05-10 01:26:16 +0600

    Update money.js

--- Script/commands/money.js
+++ Script/commands/money.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "money",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "SHAHADAT SAHU",
+    credits: "Zakariya",
     description: "Lightweight economy system",
     commandCategory: "economy",
     usages: "",
