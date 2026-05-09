07206ea3b0e8b76c2cb8b701a350950a5224f74f
JeonTisha70 <tishamyhappiness@gmail.com>
2026-05-10 02:02:36 +0600

    Update spam.js

--- Script/commands/spam.js
+++ Script/commands/spam.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "spam",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "SHAHADAT SAHU",
+  credits: "ZAKARIYA",
   description: "",
   commandCategory: "spam",
   usages: "[msg] [amount]",
