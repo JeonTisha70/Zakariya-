f872e5f68bd005bf9c42e9b04567ff2fcacc573c
JeonTisha70 <tishamyhappiness@gmail.com>
2026-05-10 00:20:27 +0600

    Update adbot.js

--- Script/commands/adbot.js
+++ Script/commands/adbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "ckbot",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔",
+    credits: "𝐙𝐀𝐊𝐀𝐑𝐈𝐘𝐀",
     description: "DESCRIPTION ABOUT BOT",
     commandCategory: "Media",
     usages: "",
