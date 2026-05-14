9c84ee82fdcde976fe0f6e4e10a15f7c179f2b98
JeonTisha70 <tishamyhappiness@gmail.com>
2026-05-15 01:40:34 +0600

    Update Autoreply.js

--- Script/commands/Autoreply.js
+++ Script/commands/Autoreply.js
@@ -42,7 +42,6 @@ const responses = {
   "good morning": "Good Morning 😚",
   "good night": "Good Night 🌙💤",
   "love you": "আমিও তোমাকে ভালোবাসি 😘",
-  "i love you": "I love you 😘💋",
   "miss you": "আমিও তোমাকে মিস করি 🥺",
   hmm: "হুম বলো জানু 😌",
   bot: "বলো কি করতে পারি 😎",
