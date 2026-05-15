ffbda742ecf20c9f47c6cb9f88e5ae86b0ba3863
JeonTisha70 <tishamyhappiness@gmail.com>
2026-05-15 13:28:30 +0600

    Update baby.js

--- Script/commands/baby.js
+++ Script/commands/baby.js
@@ -71,7 +71,7 @@ const responses = {
   "owner": "𝐎𝐖𝐍𝐄𝐑 ☞ ZAKARIYA JIYEM ☜",
   "admin": "He is ZAKARIYA 😘 সবাই Admin ARIYAN নামে চিনে ☺️",
 
-  "bot": "বলো কি করতে পারি 😎",
+  "bot": "",
   "baby": "হুম জানু 💖",
   "janu": "হুম জানু বলো 😚💖",
   "jaan": "এতো জান জান করো না 🙈💖",
